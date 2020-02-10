cf4ef541f27aab2a7c747aeed86557d6e020f175
Jose Fiascaro <53021912+jfiascaro@users.noreply.github.com>
2020-02-10 14:45:27 -0600

    Access-Control-Allow-Methods = "*"

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Methods", "*"); //Permite todos los métodos de peticiones
     next();
 });
 
